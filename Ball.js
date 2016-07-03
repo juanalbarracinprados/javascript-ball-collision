c6c0742964d315a57f61ddf937214a503e0e75f1
Juan Albarracín Prados <juan@prokom.es>
2016-07-03 14:41:11 +0200

    gen_color: string must start with "#"

--- Ball.js
+++ Ball.js
@@ -15,7 +15,7 @@ function Ball (px, py, vx, vy, radius){
 	function genColor (){
 		// Generates a html color randomly
 		var hex = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
-		var color="";
+		var color="#";
 		for (i=0;i<6;i++){
 			color+=hex[Math.round(Math.random()*15)];
 		}
